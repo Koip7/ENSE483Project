7842435441a45c8dd7d9280f1eece6b3d099e3d3
Koip7 <koiop7@gmail.com>
2019-04-10 23:01:06 -0600

    Made Distance values be calculated from RSSI, added in default configurations for the spoofer, fixed issues when calculating positions

--- ense483.bluetooth.rssi.configurable/src/ense483/bluetooth/rssi/configurable/Spoofer.java
+++ ense483.bluetooth.rssi.configurable/src/ense483/bluetooth/rssi/configurable/Spoofer.java
@@ -141,47 +141,37 @@ public class Spoofer implements ConfigurableComponent, CloudConnectionListener, 
      * Called at the configured rate to publish the next temperature measurement.
      */
     private void doPublish() {
+    	String simulation;
+    	KuraPayload payload;
+    	KuraMessage message;
+    	
         if (this.cloudPublisher == null) {
             logger.info("No cloud publisher selected. Cannot publish!");
             return;
         }
+        
+        
+        
+        simulation = (String) this.properties.get("simulation");
+        logger.info("Simulation Selected message: " + simulation);
+        switch(simulation) {
+        	case "Custom":
+        		payload = custom_packed_payload();
+        		break;
+        	case "Cross":
+        		payload = cross_packed_payload();
+        		break;
+        	case "Box":
+        		payload = box_packed_payload();
+        		break;
+        	case "Surrounded":
+        		payload = surrounded_packed_payload();
+        		break;
+        	default:
+        		payload = custom_packed_payload();
+        }
 
-        // Allocate a new payload
-        KuraPayload payload = new KuraPayload();
-
-        // Timestamp the message
-        payload.setTimestamp(new Date());
-        
-       //TODO: Do this programically instead of hardcoded
-        //packing of the RSSI data as read from the configuration data
-        payload.addMetric("01", (Integer) this.properties.get("rssi.node01"));
-        payload.addMetric("02", (Integer) this.properties.get("rssi.node02"));
-        payload.addMetric("03", (Integer) this.properties.get("rssi.node03"));
-        payload.addMetric("04", (Integer) this.properties.get("rssi.node04"));
-        payload.addMetric("05", (Integer) this.properties.get("rssi.node05"));
-        payload.addMetric("06", (Integer) this.properties.get("rssi.node06"));
-        payload.addMetric("07", (Integer) this.properties.get("rssi.node07"));
-        payload.addMetric("08", (Integer) this.properties.get("rssi.node08"));
-        payload.addMetric("09", (Integer) this.properties.get("rssi.node09"));
-        
-        payload.addMetric("12", (Integer) this.properties.get("rssi.node12"));
-        payload.addMetric("13", (Integer) this.properties.get("rssi.node13"));
-        payload.addMetric("14", (Integer) this.properties.get("rssi.node14"));
-        payload.addMetric("15", (Integer) this.properties.get("rssi.node15"));
-        payload.addMetric("16", (Integer) this.properties.get("rssi.node16"));
-        payload.addMetric("17", (Integer) this.properties.get("rssi.node17"));
-        payload.addMetric("18", (Integer) this.properties.get("rssi.node18"));
-        payload.addMetric("19", (Integer) this.properties.get("rssi.node19"));
-        
-        payload.addMetric("23", (Integer) this.properties.get("rssi.node23"));
-        payload.addMetric("24", (Integer) this.properties.get("rssi.node24"));
-        payload.addMetric("25", (Integer) this.properties.get("rssi.node25"));
-        payload.addMetric("26", (Integer) this.properties.get("rssi.node26"));
-        payload.addMetric("27", (Integer) this.properties.get("rssi.node27"));
-        payload.addMetric("28", (Integer) this.properties.get("rssi.node28"));
-        payload.addMetric("29", (Integer) this.properties.get("rssi.node29"));
-        
-        KuraMessage message = new KuraMessage(payload);
+        message = new KuraMessage(payload);
 
         // Publish the message
         try {
@@ -191,4 +181,156 @@ public class Spoofer implements ConfigurableComponent, CloudConnectionListener, 
             logger.error("Cannot publish message: {}", message, e);
         }
     }
+    
+    KuraPayload custom_packed_payload() {
+    	 // Allocate a new payload
+        KuraPayload payload = new KuraPayload();
+
+        // Timestamp the message
+        payload.setTimestamp(new Date()); 
+        //TODO: Do this programically instead of hardcoded
+         //packing of the RSSI data as read from the configuration data
+         payload.addMetric("01", (Float) this.properties.get("rssi.node01"));
+         payload.addMetric("02", (Float) this.properties.get("rssi.node02"));
+         payload.addMetric("03", (Float) this.properties.get("rssi.node03"));
+         payload.addMetric("04", (Float) this.properties.get("rssi.node04"));
+         payload.addMetric("05", (Float) this.properties.get("rssi.node05"));
+         payload.addMetric("06", (Float) this.properties.get("rssi.node06"));
+         payload.addMetric("07", (Float) this.properties.get("rssi.node07"));
+         payload.addMetric("08", (Float) this.properties.get("rssi.node08"));
+         payload.addMetric("09", (Float) this.properties.get("rssi.node09"));
+         
+         payload.addMetric("12", (Float) this.properties.get("rssi.node12"));
+         payload.addMetric("13", (Float) this.properties.get("rssi.node13"));
+         payload.addMetric("14", (Float) this.properties.get("rssi.node14"));
+         payload.addMetric("15", (Float) this.properties.get("rssi.node15"));
+         payload.addMetric("16", (Float) this.properties.get("rssi.node16"));
+         payload.addMetric("17", (Float) this.properties.get("rssi.node17"));
+         payload.addMetric("18", (Float) this.properties.get("rssi.node18"));
+         payload.addMetric("19", (Float) this.properties.get("rssi.node19"));
+         
+         payload.addMetric("23", (Float) this.properties.get("rssi.node23"));
+         payload.addMetric("24", (Float) this.properties.get("rssi.node24"));
+         payload.addMetric("25", (Float) this.properties.get("rssi.node25"));
+         payload.addMetric("26", (Float) this.properties.get("rssi.node26"));
+         payload.addMetric("27", (Float) this.properties.get("rssi.node27"));
+         payload.addMetric("28", (Float) this.properties.get("rssi.node28"));
+         payload.addMetric("29", (Float) this.properties.get("rssi.node29"));
+         
+         return payload;
+    }
+    
+    KuraPayload box_packed_payload() {
+      	 // Allocate a new payload
+          KuraPayload payload = new KuraPayload();
+
+          // Timestamp the message
+          payload.setTimestamp(new Date()); 
+          //TODO: Do this programically instead of hardcoded
+           //packing of the RSSI data as read from the configuration data
+           payload.addMetric("01", -98.54242509f);
+           payload.addMetric("02", -100.1394335f);
+           payload.addMetric("03", -89.0f);
+           payload.addMetric("04", -95.02059991f);
+           payload.addMetric("05", -99.0f);
+           payload.addMetric("06", -98.03089987f);
+           payload.addMetric("07", -95.98970004f);
+           payload.addMetric("08", -95.02059991f);
+           payload.addMetric("09", -89);
+           
+           payload.addMetric("12", -95.02059991f);
+           payload.addMetric("13", -95.02059991f);
+           payload.addMetric("14", -89.0f);
+           payload.addMetric("15", -89.0f);
+           payload.addMetric("16", -95.98970004f);
+           payload.addMetric("17", -98.03089987);
+           payload.addMetric("18", -100.1394335f);
+           payload.addMetric("19", -99.0f);
+           
+           payload.addMetric("23", -98.03089987f);
+           payload.addMetric("24", -95.98970004f);
+           payload.addMetric("25", -89.0f);
+           payload.addMetric("26", -89.0f);
+           payload.addMetric("27", -95.02059991f);
+           payload.addMetric("28", -98.54242509f);
+           payload.addMetric("29", -99.0f);
+           
+           return payload;
+      }
+    
+    KuraPayload surrounded_packed_payload() {
+     	 // Allocate a new payload
+         KuraPayload payload = new KuraPayload();
+
+         // Timestamp the message
+         payload.setTimestamp(new Date()); 
+         //TODO: Do this programically instead of hardcoded
+          //packing of the RSSI data as read from the configuration data
+          payload.addMetric("01", -94.11883361f);
+          payload.addMetric("02", -94.11883361f);
+          payload.addMetric("03", -95.2838893f);
+          payload.addMetric("04", -95.2838893f);
+          payload.addMetric("05", -94.11883361f);
+          payload.addMetric("06", -95.02059991f);
+          payload.addMetric("07", -94.11883361f);
+          payload.addMetric("08", -95.2838893f);
+          payload.addMetric("09", -95.2838893f);
+          
+          payload.addMetric("12", -95.02059991f);
+          payload.addMetric("13", -99.0f);
+          payload.addMetric("14", -100.1394335f);
+          payload.addMetric("15", -100.1394335f);
+          payload.addMetric("16", -100.2221588f);
+          payload.addMetric("17", -98.54242509f);
+          payload.addMetric("18", -95.98970004f);
+          payload.addMetric("19", -92.01029995f);
+          
+          payload.addMetric("23", -92.01029995f);
+          payload.addMetric("24", -95.98970004f);
+          payload.addMetric("25", -98.54242509f);
+          payload.addMetric("26", -101.04119988f);
+          payload.addMetric("27", -102.9794001f);
+          payload.addMetric("28", -104.563025f);
+          payload.addMetric("29", -105.9019608f);
+          
+          return payload;
+     }
+    
+    KuraPayload cross_packed_payload() {
+   	 // Allocate a new payload
+       KuraPayload payload = new KuraPayload();
+
+       // Timestamp the message
+       payload.setTimestamp(new Date()); 
+       //TODO: Do this programically instead of hardcoded
+        //packing of the RSSI data as read from the configuration data
+        payload.addMetric("01", -89.0f);
+        payload.addMetric("02", -89.0f);
+        payload.addMetric("03", -89.0f);
+        payload.addMetric("04", -95.02059991f);
+        payload.addMetric("05", -95.02059991f);
+        payload.addMetric("06", -98.54242509f);
+        payload.addMetric("07", -89.0f);
+        payload.addMetric("08", -95.02059991f);
+        payload.addMetric("09", -95.02059991f);
+        
+        payload.addMetric("12", -95.02059991f);
+        payload.addMetric("13", -92.01029995f);
+        payload.addMetric("14", -95.98970004f);
+        payload.addMetric("15", -98.54242509f);
+        payload.addMetric("16", -101.0411998f);
+        payload.addMetric("17", -92.01029995f);
+        payload.addMetric("18", -95.98970004f);
+        payload.addMetric("19", -89.0f);
+        
+        payload.addMetric("23", -92.01029995f);
+        payload.addMetric("24", -95.98970004f);
+        payload.addMetric("25", -89.0f);
+        payload.addMetric("26", -95.02059991f);
+        payload.addMetric("27", -92.01029995f);
+        payload.addMetric("28", -95.98970004f);
+        payload.addMetric("29", -98.54242509f);
+        
+        return payload;
+   }
 }
--- pyTriangulationServer/triangulationServer.py
+++ pyTriangulationServer/triangulationServer.py
@@ -13,6 +13,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import manifold
 import math
+import traceback
 
 #the number of points in the system, this value must at least be greater than 4
 NUMBER_OF_POINTS = 10
@@ -21,7 +22,7 @@ NUMBER_OF_POINTS = 10
 calculates the distance from coord1 to coord2
 """
 def distance(coord1, coord2):
-	return math.sqrt(math.pow(coord1[0] - coord2[0], 2) - math.pow(coord1[1] - coord2[1], 2))
+	return math.sqrt(math.pow(coord1[0] - coord2[0], 2) + math.pow(coord1[1] - coord2[1], 2))
 
 """
 Given a matrix defining the distances between all nodes this function will plot the coordinates of each point only depending on the distance of node 0-2 to the rest of the nodes
@@ -36,21 +37,49 @@ def find_coordinates(distanceMatrix):
 	coordinates.append([0,0])
 	#asigning the second point and making it's X equal to the distance from the first point
 	coordinates.append([distanceMatrix[0][1],0])
+	print('initialized 1st and 2nd points')
 	#detemining the angle
-	angle = math.acos((math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][2],2) - math.pow(distanceMatrix[1][2],2))/(2 * distanceMatrix[0][1]*distanceMatrix[0][2]))
-	#finding the coordinates based on the angle of the third point
-	x = distanceMatrix[0][2] * math.cos(angle)
-	y = distanceMatrix[0][2] * math.sin(angle)
-	coordinates.append([x,y])
+	try:
+		var1 = math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][2],2) - math.pow(distanceMatrix[1][2],2)
+		var2 = 2 * distanceMatrix[0][1]*distanceMatrix[0][2]
+		var3 = var1/var2
+
+		if var3 < -1:
+			var3 = -1.0
+		if var3 > 1:
+			var3 = 1.0
+		angle = math.acos(var3)
+		#angle = math.acos((math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][2],2) - math.pow(distanceMatrix[1][2],2))/(2 * distanceMatrix[0][1]*distanceMatrix[0][2]))
+		#finding the coordinates based on the angle of the third point
+		x = distanceMatrix[0][2] * math.cos(angle)
+		y = distanceMatrix[0][2] * math.sin(angle)
+		coordinates.append([x,y])
+		print('initialized 3rd point')
+	except Exception as e:
+		print(traceback.format_exc())
+		print('could not initialize 3rd point')
 	#Calculating the other coordinates
 	for nodeToCalculate in range (3, NUMBER_OF_POINTS):
-		angle = math.acos((math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][nodeToCalculate],2) - math.pow(distanceMatrix[1][nodeToCalculate],2))/(2 * distanceMatrix[0][1]*distanceMatrix[0][nodeToCalculate]))
-		x = distanceMatrix[0][nodeToCalculate] * math.cos(angle)
-		y = distanceMatrix[0][nodeToCalculate] * math.sin(angle)
-		if(abs(distance([x,y], coordinates[2]) - distanceMatrix[2][nodeToCalculate]) > abs(distance([x,-y], coordinates[2]) - distanceMatrix[2][nodeToCalculate])):
-			y=-y
-		coordinates.append([x,y])
-	#print(coordinates)
+		try:
+			#angle = math.acos((math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][nodeToCalculate],2) - math.pow(distanceMatrix[1][nodeToCalculate],2))/(2 * distanceMatrix[0][1]*distanceMatrix[0][nodeToCalculate]))
+			var1 = math.pow(distanceMatrix[0][1],2) + math.pow(distanceMatrix[0][nodeToCalculate],2) - math.pow(distanceMatrix[1][nodeToCalculate],2)
+			var2 = 2 * distanceMatrix[0][1]*distanceMatrix[0][nodeToCalculate]
+			var3 = var1/var2
+
+			if var3 < -1:
+				var3 = -1.0
+			if var3 > 1:
+				var3 = 1.0
+			angle = math.acos(var3)
+			x = distanceMatrix[0][nodeToCalculate] * math.cos(angle)
+			y = distanceMatrix[0][nodeToCalculate] * math.sin(angle)
+			if(abs(distance([x,y], coordinates[2]) - distanceMatrix[2][nodeToCalculate]) > abs(distance([x,-y], coordinates[2]) - distanceMatrix[2][nodeToCalculate])):
+				y=-y
+			coordinates.append([x,y])
+		except Exception as e:
+			print(traceback.format_exc())
+			print('could not initialized ' + str(nodeToCalculate) + ' point')
+	print(coordinates)
 	#Printing the coordinates in a graph
 	graph_coordinates(coordinates)
 	return
@@ -84,6 +113,14 @@ def on_connect(client, userdata, flags, rc):
 	return
 
 '''
+returns the distance calculated from RSSI
+'''
+def distance_from_RSSI(RSSI):
+	MEASURED_POWER = -69
+	N = 2
+	return math.pow(10, (MEASURED_POWER - RSSI)/(10 * N))
+
+'''
 function stating what to do upon successfull message recived from a mqqt broker
 '''
 def on_message(client, userdata, msg):
@@ -91,11 +128,12 @@ def on_message(client, userdata, msg):
 	distanceMatrix = np.zeros((3,NUMBER_OF_POINTS))
 	jsonMsg = str(msg.payload)	
 	loadedJson = json.loads(jsonMsg)
-	print('Done loading file')
+	print('Done loading data')
 	#setting values in the distance matrix
 	for data in loadedJson["metrics"]:
-		distanceMatrix[int(data[0])][int(data[1])] = loadedJson["metrics"][data]
-		#distanceMatrix[int(data[1])][int(data[0])] = loadedJson["metrics"][data]
+		#convert RSSI to distance
+		distanceMatrix[int(data[0])][int(data[1])] = distance_from_RSSI(float(loadedJson["metrics"][data]))
+		print('in ' + data + ' storing ' + str(loadedJson["metrics"][data]) + ' calculated ' + str(distance_from_RSSI(float(loadedJson["metrics"][data]))) + ' stored in ' + str(int(data[0])) + ',' + str(int(data[1])))
 	print('made distance matrix')
 	print(distanceMatrix)
 	find_coordinates(distanceMatrix)
